f35e94daa2aadd8314ecc34f8f393454d80a11ed
shijianjun <shijianjun@zhizukj.com>
2023-09-26 19:34:37 +0800

    fix: no-message

---
+++ packages/create-cli/src/cli.ts
@@ -0,0 +1,13 @@
+import cac from 'cac'
+import minimist from 'minimist'
+
+export async function create() {
+  try {
+    const cli = cac('create-svelte-design')
+   
+    
+  } catch (e) {
+    console.error(e)
+    process.exit(1)
+  }
+}
--- packages/create-cli/src/index.ts
+++ packages/create-cli/src/index.ts
@@ -1,21 +1,3 @@
-import cac from 'cac'
-import { version } from '../package.json'
-
-export async function create() {
-  try {
-    const cli = cac('create-svelte-design')
-    cli
-      .version(version)
-      .option('-n, --name <name>', `project name`)
-      .option('-p, --path <path>', `project path`)
-
-    const result = cli.parse()
-    const args = result.options
-    
-  } catch (e) {
-    console.error(e)
-    process.exit(1)
-  }
-}
-
-create()
+export * from './cli'
+export * from './contant'
+export * from './build'
